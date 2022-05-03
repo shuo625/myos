00023897cd2ab67bbd239d3052c3984c5afd9e5c
Shuo626 <qushuo233@gmail.com>
2022-05-03 12:14:51 +0100

    add main

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
