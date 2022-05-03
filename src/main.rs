46784676038e4103579645853083bc4810c958f4
Shuo626 <qushuo233@gmail.com>
2022-05-03 23:26:46 +0100

    implement panic function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 mod vga_buffer;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+mod panic;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World! numbers are {} and {}", 42, 1.0 / 3.0);
+    panic!("Some panic message");
 
+    #[allow(unreachable_code)]
     loop {}
 }
