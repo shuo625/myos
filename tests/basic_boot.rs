cad779622fa7e2404b139318d29822e9e4747837
Shuo626 <qushuo233@gmail.com>
2022-05-06 17:43:01 +0100

    add an integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(myos::test_runner::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use myos::println;
+use myos::panic_handler;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    panic_handler::test_panic_handler(info);
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
