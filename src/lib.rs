e266c17b6b2f891f919bd211903e4deb2001a4de
shuo625 <qushuo233@gmail.com>
2022-06-17 17:52:54 +0100

    support heap

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,14 @@
 #![test_runner(crate::test_runner::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 #[allow(unused_imports)]
 use bootloader::{entry_point, BootInfo};
 
 pub mod exit;
 pub mod gdt;
+pub mod heap;
 pub mod interrupts;
 pub mod memory;
 pub mod panic_handler;
